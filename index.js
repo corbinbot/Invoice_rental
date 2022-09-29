93588a043d81d93bc10612701bc340b84656a9a0
corbinbot <viveklawaniya98@gmail.com>
2022-09-29 12:48:31 +0530

    v

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 var s_no = 1;
-var text1 = document.getElementById("num1");
-var text2 = document.getElementById("num2");
 $('document').ready(function() {
   $('.add_another').click(function() {
     if (s_no <= 20) {
       s_no++;
     }
 
-    $("#tbl").append('<tr><th>' + s_no + '.</th><td><input type="date" name="date" class="form-control" placeholder="Date" required autofocus></td><td><input type="Description" name="Description" class="form-control" placeholder="Enter Description" required></td><td><input id="Amount' + s_no + '" type="number" name="Amount class="form-control" placeholder="Enter Amount" oninput="add_number()" required ></td><td><input id="Tax' + s_no + '" type="number" name="Taxes" class="form-control" placeholder="Enter Tax" oninput="add_number()" required></td><td><input type="text" class="form-control" id="txtresult' + s_no + '" name="TextBox3"></td></tr>');
+    $("#tbl").append('<tr><th>' + s_no + '.</th><td><input type="number" name="date" class="form-control" placeholder="Quantity" required autofocus></td><td><input type="Description" name="Description" class="form-control" placeholder="Enter Description"></td><td><input id="Amount' + s_no + '" type="number" name="Amount" class="form-control" placeholder="Enter Amount" oninput="add_number()"></td><td><input id="Tax' + s_no + '" type="number" name="Taxes" class="form-control" placeholder="Enter Price" oninput="add_number()"></td><td><input type="text" class="form-control" id="txtresult' + s_no + '" name="TextBox3"></td></tr>');
   });
 })
 
-var text1 = document.getElementById("Amount" + s_no);
-var text2 = document.getElementById("Tax" + s_no);
-
 function add_number() {
-   var first_number = parseFloat(text1.value);
-   if (isNaN(first_number)) first_number = 0;
-   var second_number = parseFloat(text2.value);
-   if (isNaN(second_number)) second_number = 0;
-   var result = first_number + second_number;
-   document.getElementById("txtresult" + s_no).value = result;
+  var text1 = document.getElementById("Amount" + s_no);
+  var text2 = document.getElementById("Tax" + s_no);
+
+  var first_number = parseFloat(text1.value);
+  if (isNaN(first_number)) first_number = 0;
+  var second_number = parseFloat(text2.value);
+  if (isNaN(second_number)) second_number = 0;
+  var result = first_number + second_number;
+  document.getElementById("txtresult" + s_no).value = result;
 }
